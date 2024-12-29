ac1d7229ab7af45d9a371323c4b4181b622dae32
iakov <yashahelfi2012@gmail.com>
2024-12-29 10:59:48 +0200

    Bump version: 1.6.6 → 1.6.7

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -11,7 +11,7 @@ warnings.filterwarnings(
 )
 
 __all__ = ["Database", "Table", "connect"]
-__version__ = "1.6.6"
+__version__ = "1.6.7"
 
 
 def connect(
--- docs/conf.py
+++ docs/conf.py
@@ -45,9 +45,9 @@ copyright = u"2013-2021, Friedrich Lindenberg, Gregor Aisch, Stefan Wehrmeyer"
 # built documents.
 #
 # The short X.Y version.
-version = "1.6.6"
+version = "1.6.7"
 # The full version, including alpha/beta/rc tags.
-release = "1.6.6"
+release = "1.6.7"
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
